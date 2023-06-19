1fb657338c75cd4235c8f92e94b929d98682d96e
firuz.azizbekov <firuz.azizbekov_2026@ucentralasia.org>
2023-06-19 14:24:45 +0500

    .

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,8 +1,8 @@
 package server
 
 import (
-	"first/internal/api"
 	"first/internal/middleware"
+	"first/pkg/handlers"
 	"fmt"
 	"github.com/gorilla/mux"
 	"log"
@@ -12,10 +12,10 @@ import (
 // func Run(address string, router http.Handler) {
 func Run() {
 	r := mux.NewRouter().StrictSlash(true)
-	r.HandleFunc("/add", middleware.Middleware(http.HandlerFunc(api.Add)))
-	r.HandleFunc("/sub", middleware.Middleware(http.HandlerFunc(api.Subtraction)))
-	r.HandleFunc("/mul", middleware.Middleware(http.HandlerFunc(api.Multiplication)))
-	r.HandleFunc("/div", middleware.Middleware(http.HandlerFunc(api.Division)))
+	r.HandleFunc("/add", middleware.Middleware(http.HandlerFunc(handlers.Add)))
+	r.HandleFunc("/sub", middleware.Middleware(http.HandlerFunc(handlers.Subtraction)))
+	r.HandleFunc("/mul", middleware.Middleware(http.HandlerFunc(handlers.Multiplication)))
+	r.HandleFunc("/div", middleware.Middleware(http.HandlerFunc(handlers.Division)))
 	fmt.Printf("The server is started!!!\n")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "root"
+	dbname   = "postgres"
+)
+
 func main() {
-	//Run()
+	db, err := sql.Open("postgres", fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname))
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	fmt.Println("Connected to PostgreSQL!")
 }
